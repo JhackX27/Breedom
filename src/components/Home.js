aa299bd3583d9ad5cfb9b5681ced2c4a9001943b
jhackx27 <jhack.x27@outlook.com>
2024-08-30 09:55:16 -0500

    home added

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="text-center">
       <h1 className="text-4xl font-bold mb-4">Bienvenido a Breedom</h1>
-      <p className="text-xl">Juega, gana puntos y canjea servicios psicológicos.</p>
+      <p className="text-xl">Una comunidad para divertirte y aprender mediante juegos y actividades lúdicas</p>
     </div>
   );
 }
